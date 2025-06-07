0876b70fa3407337789b4351483db11526c28324
alyssa-embrace <alyssa.syharath@gmail.com>
2025-06-06 22:51:01 -0400

    Removing one write

--- .github/scripts/gmtl_to_junit.py
+++ .github/scripts/gmtl_to_junit.py
@@ -1,6 +1,13 @@
 import argparse
+
 print("hi hi from gmtl to junit")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('results_file_path')
-    .add_argument('out_file_path')
+parser.add_argument('out_file_path')
+
+args = parser.parse_args()
+
+with open(args.results_file_path) as f:
+    contents = f.read()
+    print(contents)
